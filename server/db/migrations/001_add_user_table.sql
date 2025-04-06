581b5d5c023ef14e63e1aa061628fdb1a436c59c
Htet Aung Khant <htetaung23.ha@gmail.com>
2025-04-06 23:51:20 +0630

    feat(migration): add createdAt and updatedAt to create user table

--- server/db/migrations/001_add_user_table.sql
+++ server/db/migrations/001_add_user_table.sql
@@ -3,7 +3,9 @@ CREATE TABLE users (
     id UUID PRIMARY KEY,
     username VARCHAR(32) NOT NULL,
     email VARCHAR(32) NOT NULL,
-    password VARCHAR(32) NOT NULL
+    password VARCHAR(32) NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
 );
 
 -- +goose Down
