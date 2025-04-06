87ffa7abbb1aee2a48e792419bede4f738d6e83c
Htet Aung Khant <htetaung23.ha@gmail.com>
2025-04-07 00:14:09 +0630

    feat(user): implement repository and createUser method

---
+++ server/internal/user/user.go
@@ -0,0 +1,21 @@
+package user
+
+import (
+	"context"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	UserName  string    `json:"username" db:"username"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"password" db:"password"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
+
+type Repository interface {
+	CreateUser(ctx context.Context, user *User) (*User, error)
+}
---
+++ server/internal/user/user_repository.go
@@ -0,0 +1,49 @@
+package user
+
+import (
+	"context"
+	"database/sql"
+)
+
+type DBTX interface {
+	ExecContext(context.Context, string, ...interface{}) (sql.Result, error)
+	PrepareContext(context.Context, string) (*sql.Stmt, error)
+	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
+	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
+}
+
+type repository struct {
+	db DBTX
+}
+
+func NewRepository(db DBTX) Repository {
+	return &repository{db: db}
+}
+
+func (repo *repository) CreateUser(ctx context.Context, user *User) (*User, error) {
+	var newUser User
+	query := `INSERT INTO users(username, email, password, created_at, updated_at) VALUES($1, $2, $3, $4, $5) RETURNING *`
+
+	err := repo.db.QueryRowContext(
+		ctx,
+		query,
+		user.UserName,
+		user.Email,
+		user.Password,
+		user.CreatedAt,
+		user.UpdatedAt,
+	).Scan(
+		&newUser.ID,
+		&newUser.UserName,
+		&newUser.Email,
+		&newUser.Password,
+		&newUser.CreatedAt,
+		&newUser.UpdatedAt,
+	)
+
+	if err != nil {
+		return &User{}, err
+	}
+
+	return &newUser, nil
+}
