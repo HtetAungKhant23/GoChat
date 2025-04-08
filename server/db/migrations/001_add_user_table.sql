171c15085aab629deb9723f19fce5f93210e27c6
Htet Aung Khant <htetaung23.ha@gmail.com>
2025-04-08 22:27:22 +0630

    fix(sql): change varchar 32 to 255 in users table

--- server/db/migrations/001_add_user_table.sql
+++ server/db/migrations/001_add_user_table.sql
@@ -1,9 +1,9 @@
 -- +goose Up
 CREATE TABLE users (
     id UUID PRIMARY KEY,
-    username VARCHAR(32) NOT NULL,
-    email VARCHAR(32) NOT NULL,
-    password VARCHAR(32) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP NOT NULL
 );
